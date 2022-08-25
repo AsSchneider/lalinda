4793033859be7d8e2e93cb791f811874a3f403cf
astridschneider <astrid_s83@hotmail.com>
2022-08-24 22:34:19 -0300

    primercommit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+let nombre = prompt("ingrese el nombre de mi perra");
+
+while(nombre != "india"){
+    alert ("usted no es su veterinario");
+    nombre = prompt("ingrese el nombre de mi perra");
+    
+}
+alert("veterinario correcto");
+
+/*for (let i = 1; i <= 10; i++){
+    console.log (`aprendiendofor ${i}`);
+}
+console.log("fin de for");*/
+
+  
+
+
+
